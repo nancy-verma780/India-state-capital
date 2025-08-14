1e44063563934ef1312b2d2e9486cfc9aaa8cd34
nancy-verma780 <nancy45815@gmail.com>
2025-08-14 16:03:28 +0530

    State & Capital

---
+++ India_Map.py
@@ -0,0 +1,100 @@
+print("Write India state name to get the capital")
+# Write your state as an input
+state = input("Enter your state: ").lower()
+#state = state.lower()
+print(state)
+
+
+if(state == "andhra pradesh"):
+    print("Hyderabad")
+
+elif(state == "arunachal pradesh"):
+    print("Itanagar")
+
+elif(state == "assam"):
+    print("Dispur")
+
+elif(state == "bihar"):
+    print("Patna")
+
+elif(state == "chhatisgarh"):
+    print("Raipur")
+
+elif(state == "goa"):
+    print("Panaji")
+
+elif(state == "gujrat"):
+    print("Gandhi nagar")
+
+elif(state == "haryana"):
+    print("Chandhigarh")
+
+elif(state == "himachal pradesh"):
+    print("Shimla")
+
+elif(state == "jammu and kashmir"):
+    print("Srinagar")
+
+elif(state == "jharkhand"):
+    print("Ranchi")
+
+elif(state == "karnatka"):
+    print("Bengalaru")
+
+elif(state == "kerala"):
+    print("Thrivanthampuram")
+
+elif(state == "madhya pradesh"):
+    print("Bhopal")
+
+elif(state == "maharashtra"):
+    print("Mumbai")
+
+elif(state == "manipur"):
+    print("Imphal")
+
+elif(state == "meghalaya"):
+    print("Shillong")
+
+elif(state == "mizoram"):
+    print("Aizwal")
+
+elif(state == "nagaland"):
+    print("Kohima")
+
+elif(state == "odisha"):
+    print("Bubaneshwar")
+
+elif(state == "punjab"):
+    print("Chandigarh")
+
+elif(state == "rajasthan"):
+    print("Jaipur")
+
+elif(state == "sikkim"):
+    print("Gangtok")
+
+elif(state == "tamil nadu"):
+    print("Chennai")
+
+elif(state == "telangana"):
+    print("Hyderabad")
+
+elif(state == "tripura"):
+    print("Agartala")
+
+elif(state == "uttar pradesh"):
+    print("Lucknow")
+
+elif(state == "uttrakhand"):
+    print("Dehradun")
+
+elif(state == "west bengal"):
+    print("Kolkata")
+
+else:
+    print("You Didn't write the India State name.\nPlease Enter India State correctly.")
+
+
+    
+
